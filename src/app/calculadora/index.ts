8b5bca5971e3a831ffb3f46cf0766e0c34b18706
gabrielfurtadon <gabrielfurtado@alunos.utfpr.edu.br>
2023-05-31 11:50:35 -0300

    calculadora Service

--- src/app/calculadora/index.ts
+++ src/app/calculadora/index.ts
@@ -1,2 +1,4 @@
 // CLASSE SE TORNA PUBLICA AO ACESSAR O DIRETÓRIO (NÃO PRECISA PASSAR O MODULO TODO DAI)
 export * from './calculadora.module';
+export * from './components'
+export * from './services'
