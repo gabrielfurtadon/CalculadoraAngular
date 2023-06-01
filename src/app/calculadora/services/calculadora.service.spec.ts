b9dafcf72b2503cc4bc1e4c8ec7f4fd0e520da10
gabrielfurtadon <gabrielfurtado@alunos.utfpr.edu.br>
2023-06-01 17:20:33 -0300

    calculadora tests

--- src/app/calculadora/services/calculadora.service.spec.ts
+++ src/app/calculadora/services/calculadora.service.spec.ts
@@ -13,4 +13,31 @@ describe('CalculadoraService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('deve garantir que 1+4 = 5', () => {
+      const soma = service.calcular(1, 4, CalculadoraService.SOMA);
+      expect(soma).toEqual(5);
+    });
+
+  it('deve garantir que 1 - 4 = -3', () => {
+    const sub = service.calcular(1,4, CalculadoraService.SUBTRACAO);
+    expect(sub).toEqual(-3);
+  });
+
+  it('deve garantir que 1 / 4 = 0.25', () => {
+    const div = service.calcular(1, 4, CalculadoraService.DIVISAO);
+    expect(div).toEqual(0.25);
+  });
+
+  it('deve garantir que 1 * 4 = 4', () => {
+    const mult = service.calcular(1, 4, CalculadoraService.MULTIPLICACAO);
+    expect(mult).toEqual(4);
+  });
+
+  it('deve garantir que retornara 0 para uma operacao invalida', () => {
+    const inv = service.calcular(1, 4, '%');
+    expect(inv).toEqual(0);
+  });
+
+
 });
--- src/app/calculadora/services/calculadora.service.ts
+++ src/app/calculadora/services/calculadora.service.ts
@@ -5,5 +5,33 @@ import { Injectable } from '@angular/core';
 })
 export class CalculadoraService {
 
+  static readonly SOMA: string = '+'; // reaonly = constante. static = pode chamar sem fazer instancia da classe
+  static readonly SUBTRACAO: string = '-';
+  static readonly DIVISAO: string = '/';
+  static readonly MULTIPLICACAO: string = '*';
+
   constructor() { }
+
+  calcular(n1: number, n2: number, operacao: string): number { // :number = retorno
+    let resultado : number;
+
+    switch(operacao) {
+      case CalculadoraService.SOMA:
+        resultado = n1 + n2;
+      break;
+      case CalculadoraService.SUBTRACAO:
+        resultado = n1 - n2;
+      break;
+      case CalculadoraService.DIVISAO:
+        resultado = n1 / n2;
+      break;
+      case CalculadoraService.MULTIPLICACAO:
+        resultado = n1 * n2;
+      break;
+      default:
+        resultado = 0;
+    }
+    return resultado;
+  }
+
 }
