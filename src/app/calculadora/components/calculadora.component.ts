91c5303c29282dfd96c286eabc9e590f121d62a6
gabrielfurtadon <gabrielfurtado@alunos.utfpr.edu.br>
2023-06-02 15:03:03 -0300

    calculadora pronta sem style

--- src/app/calculadora/components/calculadora.component.ts
+++ src/app/calculadora/components/calculadora.component.ts
@@ -10,9 +10,87 @@ import { CalculadoraService } from '../services';
 })
 export class CalculadoraComponent implements OnInit{
 
+  private num1: string;
+  private num2: string;
+  private resultado: number;
+  private operacao: string;
+
   constructor(private calculadoraService: CalculadoraService) {}
 
   ngOnInit(){
+    this.limpar();
   }
+
+  limpar() {
+    this.num1 = '0';
+    this.num2 = null;
+    this.resultado = null;
+    this.operacao = null;
+  }
+
+  adicionarNumero(numero : string): void {
+    if(this.operacao === null) {
+      this.num1 = this.concatenarNumero(this.num1, numero);
+    }else {
+      this.num2 = this.concatenarNumero(this.num2, numero);
+    }
+  }
+
+  concatenarNumero(numAtual: string, numConcat: string): string {
+    if(numAtual === '0' || numAtual === null) {
+      numAtual = '';
+    }
+
+    if(numConcat === '.' && numAtual === '') {
+      return '0.';
+    }
+
+    if(numConcat === '.' && numAtual.indexOf('.') > -1) {
+      return numAtual;
+    }
+
+    return numAtual + numConcat;
+  }
+
+
+  definirOperacao(operacao: string): void {
+    if(this.operacao === null) {
+      this.operacao = operacao;
+      return;
+    }
+
+    if(this.num2 !== null) {
+      this.resultado = this.calculadoraService.calcular(
+        parseFloat(this.num1),
+        parseFloat(this.num2),
+        this.operacao);
+      this.operacao = operacao;
+      this.num1 = this.resultado.toString();
+      this.num2 = null;
+      this.resultado = null;
+    }
+  }
+
+  calcular(): void {
+    if(this.num2 === null) {
+      return;
+    }
+
+    this.resultado = this.calculadoraService.calcular(
+      parseFloat(this.num1),
+      parseFloat(this.num2),
+      this.operacao);
+  }
+
+  get display(): string {
+    if(this.resultado !== null) {
+      return this.resultado.toString();
+    }
+    if(this.num2 !== null) {
+      return this.num2;
+    }
+    return this.num1;
+  }
+
 
 }
--- src/app/calculadora/services/calculadora.service.spec.ts
+++ src/app/calculadora/services/calculadora.service.spec.ts
@@ -40,4 +40,5 @@ describe('CalculadoraService', () => {
   });
 
 
+
 });
